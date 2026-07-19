369f32bef4753a1ad675cdebc16cdd72ae23d7e8
JiaYingWang <cwhisme@qq.com>
2026-07-19 09:22:26 +0800

    对话页 AI 的工具调用卡片容器越界问题

--- UiharuMind/UiharuMind/XmlnsDefinition.cs
+++ UiharuMind/UiharuMind/XmlnsDefinition.cs
@@ -1,3 +1,4 @@
 using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "UiharuMind.Views.Controls")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "UiharuMind.ViewModels.Converters")]
